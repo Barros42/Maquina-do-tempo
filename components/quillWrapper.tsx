142e1b726e487cffd0c217538e84474f39774eb1
Barros <mdbf42@gmail.com>
2022-02-23 11:19:16 -0300

    Remove Quill

--- components/quillWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic'
-import 'react-quill/dist/quill.snow.css'
-
-const QuillNoSSRWrapper = dynamic(import('react-quill'), {	
-	ssr: false,
-	loading: () => <p>Loading ...</p>,
-})
-
-export default QuillNoSSRWrapper
